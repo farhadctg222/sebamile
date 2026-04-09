0c755237035f122981479bf79aab47fb527bee68
farhadctg222 <farhadctg222@gmail.com>
2026-04-09 14:53:05 +0600

    d

--- app/componets/Sponsors.jsx
+++ app/componets/Sponsors.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Sponsors() {
   const [sponsors, setSponsors] = useState([]);
@@ -13,30 +14,36 @@ export default function Sponsors() {
     fetchSponsors();
   }, []);
 
-  if (!sponsors.length) return null; // loading বা no data
+  if (!sponsors.length) return null;
 
   return (
-    <section className="bg-gray-100 ">
+    <section className="bg-gray-100 overflow-hidden py-10">
       <div className="max-w-6xl mx-auto px-6">
         <h2 className="text-3xl font-bold text-center text-green-600 mb-8">
           Our Sponsors
         </h2>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 items-center justify-center">
-          {sponsors.map((sponsor) => (
-            <a
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+          {sponsors.map((sponsor, index) => (
+            <motion.a
               key={sponsor.id}
               href={sponsor.website || "#"}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center justify-center p-4 bg-white rounded-xl shadow hover:scale-105 transition"
+              initial={{ opacity: 0, x: 100 }}   // 👉 right side
+              whileInView={{ opacity: 1, x: 0 }} // 👉 center
+              transition={{ duration: 0.6, delay: index * 0.15, ease: "easeOut" }}
+              viewport={{ once: true }} // একবারই animation হবে
+              className="bg-white rounded-xl shadow p-4 flex items-center justify-center"
             >
+              <div className="w-full h-56 flex items-center justify-center">
               <img
                 src={sponsor.logo}
                 alt={sponsor.name}
-                className="max-h-16 object-contain"
-              />
-            </a>
+                className="max-h-full max-w-full object-contain"
+                />
+                      </div>
+            </motion.a>
           ))}
         </div>
       </div>
