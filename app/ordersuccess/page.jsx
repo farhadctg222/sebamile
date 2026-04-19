41788f87e63c4aa7cb8734842e435b00fcef8df6
farhadctg222 <farhadctg222@gmail.com>
2026-04-19 19:18:16 +0600

    d

--- app/ordersuccess/page.jsx
+++ app/ordersuccess/page.jsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { Suspense } from "react";
-import OrderSuccessContent from "../../componets/OrderSuccessContent";
+import OrderSuccessContent from "./OrderSuccessContent/page";
+
+
 export default function OrderSuccessPage() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <OrderSuccessContent />
+    <OrderSuccessContent></OrderSuccessContent>
     </Suspense>
   );
 }
